80edbd91a33c37c5168527e0dd3de443d06ee4b9
Serg1oGD <kanekiyayato@outlook.com>
2026-02-28 17:29:11 -0600

    Para que funciones en github como en local

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Electricidad-y-Electronica/'
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/Electricidad-y-Electronica' : '/'
 })
